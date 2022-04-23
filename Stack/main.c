4bd47288ee484776ea50c6c46402e7eea6130c18
KIM JEONGJUN <kimjeongjun@KIMs-MacBook-Pro.local>
2022-04-23 14:52:05 +0900

    ALStack comp

--- Stack/main.c
+++ Stack/main.c
@@ -1,5 +1,25 @@
+#include "../includes/ALStack.h"
+#include "../includes/LLStack.h"
+
+void testSpaceLLStack(void) {
+
+}
+
+void testSpaceALStack(void) {
+    ALStackBucket *als = createALStackBucket(3);
+    
+    pushALStackData(als, 10);
+    pushALStackData(als, 20);
+    pushALStackData(als, 30);
+    displayALStack(als);
+    pushALStackData(als, 40);
+    displayALStack(als);
+    deleteALStack(als);
+}
 
 int main(void) {
-    printf("stack hello");
+    // testSpaceALStack();
+    testSpaceLLStack();
+    system("leaks test");
     return 0;
 }
