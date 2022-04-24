30f48f9ad0f8bb56c88ddd5a363ccdb14e9bb107
djeeee1272@gmail.com <djeeee1272@gmail.com>
2022-04-24 18:39:26 +0900

    stack comp

--- Stack/expressionParser/src/expressionParser.c
+++ Stack/expressionParser/src/expressionParser.c
@@ -13,89 +13,6 @@ static bool isCloserBlank(const char input) {
     return (input == ')');
 };
 
-char operators[] = "+-*/()";
-int precedence[] = {1,1,2,2,-1,-1};
-
-char *convertInfixToPostFixWithBlank(const char *expr, size_t expr_len) {
-    if (isClosedBlank(expr, expr_len) == false) return NULL;
-    char *ret = (char *)calloc(sizeof(char), expr_len + 1);
-    if (!ret) return NULL;
-    ret[expr_len] = '\0';
-    FixedArrayStack *pStack = createFixedArrayStack(expr_len);
-    if (!pStack) {
-        free(ret);
-        return NULL;
-    }
-    size_t j = 0;
-    FASNode *nptr;
-    printf("len = %zu\n", expr_len);
-    for (size_t i = 0; i < expr_len; j++) {
-        if (isOpenerBlank(expr[j]) == true) {
-            nptr = createFASNode(expr[j]);
-            pushFAS(pStack, *nptr);
-            free(nptr);
-        } else if (isOperator(expr[j]) == true) {
-            nptr = createFASNode(expr[j]);
-            pushFAS(pStack, *nptr);
-            free(nptr);
-        } else {
-            if (isCloserBlank(expr[j]) == true) {
-                while (isFixedArrayStackEmpty(pStack) == false) {
-                    nptr = popFAS(pStack);
-                    if (isOpenerBlank(nptr->data) == true) {
-                        free(nptr);
-                        break ;
-                    }
-                    ret[i++] = nptr->data;
-                    free(nptr);          
-                }
-            } else if (isOperator(expr[j + 1]) == true || j + 1 == expr_len) {
-                if (isFixedArrayStackEmpty(pStack) == false) {
-                    nptr = popFAS(pStack);
-                    free(nptr);
-                }
-            }
-            if (isCloserBlank(expr[j]) == false) ret[i++] = expr[j];
-        }
-        printf("%s\n", ret);
-    }
-    return ret;
-}
-
-
-char *convertInfixToPostFixWithoutBlank(const char *expr, size_t expr_len) {
-    if (isClosedBlank(expr, expr_len) == false) return NULL;
-    char *ret = (char *)calloc(sizeof(char), expr_len + 1);
-    if (!ret) return NULL;
-    ret[expr_len] = '\0';
-    FixedArrayStack *pStack = createFixedArrayStack(expr_len);
-    if (!pStack) {
-        free(ret);
-        return NULL;
-    }
-    size_t j = 0;
-    FASNode *nptr;
-    printf("len = %zu\n", expr_len);
-    for (size_t i = 0; i < expr_len; j++) {
-        if (isOperator(expr[j]) == true) {
-            nptr = createFASNode(expr[j]);
-            pushFAS(pStack, *nptr);
-            free(nptr);
-        } else {
-            ret[i++] = expr[j];
-            if (isOperator(expr[j + 1]) == true || j + 1 == expr_len) {
-                if (isFixedArrayStackEmpty(pStack) == false) {
-                    nptr = popFAS(pStack);
-                    ret[i++] = nptr->data;
-                    free(nptr);
-                }
-            }
-        }
-        printf("%s\n", ret);
-    }
-    return ret;
-}
-
 bool isSuperbOperator(const char cur, const char top) {
     return ((cur == '*' || cur == '/') && (top == '+' || top == '-'));
 }
@@ -127,12 +44,6 @@ char *convertInfixToPostFix(const char *expr, size_t expr_len) {
                     pushFAS(pStack, *nptr);
                     free(nptr);
                 } else {
-                    // while (isFixedArrayStackEmpty(pStack) == false
-                    //     && isSuperbOperator(expr[j], peekFAS(pStack)->data) == false) {
-                    //     nptr = popFAS(pStack);
-                    //     ret[i++] = nptr->data;
-                    //     free(nptr);
-                    // }
                     nptr = popFAS(pStack);
                     ret[i++] = nptr->data;
                     free(nptr);
@@ -159,17 +70,8 @@ char *convertInfixToPostFix(const char *expr, size_t expr_len) {
     while (peekFAS(pStack)){
         nptr = popFAS(pStack);
         ret[i++] = nptr->data;
-        // printf("stack data : %c\n", nptr->data);
         free(nptr);
     }
     ret[i] = '\0';
-    // printf("%s |   i %2zu\n", ret, i);
     return (ret);
 }
-
-// A+B -> AB+
-// A-B -> AB-
-// 연산자 / 피연산자
-
-// A*B
-// A/B
