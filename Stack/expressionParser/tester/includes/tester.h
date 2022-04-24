438777d5934e2ff86b8b48d51011fa8e1e08c29f
djeeee1272@gmail.com <djeeee1272@gmail.com>
2022-04-24 15:05:13 +0900

    push

---
+++ Stack/expressionParser/tester/includes/tester.h
@@ -0,0 +1,10 @@
+#ifndef _TESTER_H_
+#define _TESTER_H_
+
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+bool testfield(const char *input, const bool expected);
+
+#endif // _TESTER_H_
