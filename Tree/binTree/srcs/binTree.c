e65d3ac0f58d61af7ac4db648f43b83e2c8d8141
KIM JEONGJUN <kimjeongjun@KIMs-MacBook-Pro.local>
2022-05-10 15:12:43 +0900

    binTree init

---
+++ Tree/binTree/srcs/binTree.c
@@ -0,0 +1,34 @@
+#include "../includes/binTree.h"
+#include <stdlib.h>
+
+BinTree* makeBinTree(BinTreeNode rootNode) {
+    return (BinTree *)calloc(sizeof(BinTree), 1);
+};
+
+BinTreeNode* getRootNodeBT(BinTree* pBinTree) {
+
+};
+
+BinTreeNode* insertLeftChildNodeBT(BinTreeNode* pParentNode, BinTreeNode element) {
+
+};
+
+BinTreeNode* insertRightChildNodeBT(BinTreeNode* pParentNode, BinTreeNode element) {
+
+};
+
+BinTreeNode* getLeftChildNodeBT(BinTreeNode* pNode) {
+
+};
+
+BinTreeNode* getRightChildNodeBT(BinTreeNode* pNode) {
+
+};
+
+void deleteBinTree(BinTree* pBinTree) {
+
+};
+
+void deleteBinTreeNode(BinTreeNode* pNode) {
+
+};
---
+++ Tree/binTree/srcs/main.c
@@ -0,0 +1,5 @@
+#include "../includes/treeTester.h"
+
+int main(void) {
+    return 0;
+}
